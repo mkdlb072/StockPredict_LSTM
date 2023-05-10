f412034ea7f7970c5f2cd4fcc31baa015f52396c
llano1025 <hwf1025@hotmail.com>
2023-05-11 00:54:23 +0800

    Update stock_gui.py

--- stock_gui.py
+++ stock_gui.py
@@ -1,16 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import sys
-import os
-import numpy as np
-import yfinance as yf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, Input, concatenate
-from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
 import pandas as pd
-import pandas_ta as ta
 from datetime import datetime, timedelta
 from sklearn.model_selection import train_test_split
 import stock_lib
@@ -27,42 +18,6 @@ class ConsoleOutput:
         self.textbox.configure(state='normal')
         self.textbox.insert('end', text)
         self.textbox.configure(state='disabled')
-
-
-def prepare_data(data, window_size, offset_value):
-    x = []
-    y = []
-    for i in range(offset_value, len(data) - window_size - 1):
-        window = data[i:(i + window_size), :]
-        x.append(window)
-        y.append(data[i + window_size, :])
-    x = np.array(x)
-    y = np.array(y)
-    return x, y
-
-
-def prepare_data2(data, window_size, offset_value):
-    x1, x2, x3 = ([] for r in range(3))
-    y = []
-    for i in range(offset_value + window_size * 3, len(data) - 1):
-        w1 = data[i - window_size:i, :]
-        x1.append(w1)
-        w2 = data[i - window_size * 2:i, :]
-        x2.append(w2)
-        w3 = data[i - window_size * 3:i, :]
-        x3.append(w3)
-        y.append(data[i, :])
-    x1 = np.array(x1)
-    x2 = np.array(x2)
-    x3 = np.array(x3)
-    y = np.array(y)
-    return x1, x2, x3, y
-
-
-# Splitting the data for LSTM model
-def split_data(x_input, y_input, split_ratio):
-    (x_train, x_test, y_train, y_test) = train_test_split(x_input, y_input, test_size=split_ratio, shuffle=False)
-    return x_train, x_test, y_train, y_test
 
 
 def list_industry_update(*args):
@@ -91,228 +46,92 @@ def search():
 
 def submit():
     # Load the input from gui
-    stock_name = input_box_stock_name.get()
+    stock_symbol = input_box_stock_name.get()
     delta_time = int(input_box_delta_time.get())
-    flag_train_data = var_train_data.get()
-    window_size = int(input_box_window_size.get())
-    train_epochs = int(input_box_train_epochs.get())
-    flag_lstm_algo = int(var_lstm_algo.get())
+    flg_train_data = var_train_data.get()
+    data_window = int(input_box_window_size.get())
+    epoch = int(input_box_train_epochs.get())
+    flg_lstm_algo = int(var_lstm_algo.get())
+    predict_window = 100
+    flg_train_model = 1
+    dev_env = 'NB'
 
-    # Load the stock market data from Yahoo Finance
+    # Get stock and earning data from yFinance
     start_date = datetime.now() - timedelta(days=delta_time)
     end_date = datetime.now()
-    raw_data = yf.download(stock_name, start=start_date, end=end_date)
+    raw_data = stock_lib.get_stock_data(stock_symbol, start_date, end_date)
+    np_data = raw_data.to_numpy()
+    earning_table, earning_next, earning_delta = stock_lib.get_earning_data(stock_symbol)
 
-    # Prepare technical indicators for LSTM model
-    df = pd.DataFrame(raw_data[['Close', 'Volume']])
-    df['EMA10'] = ta.ema(df['Close'], length=10)
-    df['SMA20'] = ta.sma(df['Close'], length=20)
-    df['RSI14'] = ta.rsi(df['Close'], length=10)
-    # df.index = df.index.astype(str).str.split('T').str[0]
-    # df.index = pd.to_datetime(df.index, format='%Y-%m-%d')
-    # df.index = df.index.strftime('%d-%m-%Y')
-    # df_merged = pd.merge(df, df1, how='outer', left_index=True, right_index=True)
-    # df = df_merged.loc[df.index]
-    df1, earning_next, earning_delta = stock_lib.get_earning_data(stock_name)
+    # if flg_train_data == 'Close':
+    #     features = ['Close']
+    # elif flg_train_data == 'Close, Volume':
+    #     features = ['Close', 'Volume']
+    # elif flg_train_data == 'Close, Volume, RSI14':
+    #     features = ['Close', 'Volume', 'RSI14']
+    # elif flg_train_data == 'Close, Volume, EMA10, RSI14':
+    #     features = ['Close', 'Volume', 'EMA10', 'RSI14']
+    # elif flg_train_data == 'Close, Volume, EMA10, SMA20, RSI14':
+    #     features = ['Close', 'Volume', 'EMA10', 'SMA20', 'RSI14']
 
-    if flag_train_data == 'Close':
-        features = ['Close']
-    elif flag_train_data == 'Close, Volume':
-        features = ['Close', 'Volume']
-    elif flag_train_data == 'Close, Volume, RSI14':
-        features = ['Close', 'Volume', 'RSI14']
-    elif flag_train_data == 'Close, Volume, EMA10, RSI14':
-        features = ['Close', 'Volume', 'EMA10', 'RSI14']
-    elif flag_train_data == 'Close, Volume, EMA10, SMA20, RSI14':
-        features = ['Close', 'Volume', 'EMA10', 'SMA20', 'RSI14']
+    if flg_lstm_algo == 0:
+        # Split the data for training and testing
+        x, y = stock_lib.prepare_data(np_data, data_window)
+        (x_train, x_test, y_train, y_test) = train_test_split(x, y, test_size=0.3)
+        # Scaling the training data
+        (x_train, x_test, y_train, y_test, sc) = stock_lib.minmaxscale_3d(x_train, x_test, y_train, y_test)
+        # Train the model
+        model = stock_lib.define_lstm_dnn_model(x_train)
+        model = stock_lib.train_save_model(model, x_train, y_train, x_test, y_test, epoch, flg_train_model,
+                                           stock_symbol, dev_env)
+        predicted_p = stock_lib.get_past_prediction(model, np_data, predict_window, data_window, sc)
+        predicted_f = stock_lib.get_future_prediction(model, np_data, predict_window, data_window, sc)
+        print(predicted_p[:, 0])
 
-    # Prepare data for LSTM model
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    if isinstance(flag_train_data, int):
-        data = scaler.fit_transform(df)
-    else:
-        data = scaler.fit_transform(df[features])
-    offset_value = 20  # to remove the NaN value of SMA20
-
-    if flag_lstm_algo == 0:
-        x, y = prepare_data(data, window_size, offset_value)
-        (x_train, x_test, y_train, y_test) = split_data(x, y, 0.3)
-
-        # Define function to create LSTM-DNN model
-        model = Sequential()
-        model.add(LSTM(64, return_sequences=True, input_shape=(x_train.shape[1], x_train.shape[2])))
-        model.add(Dropout(0.2))
-        model.add(LSTM(64, return_sequences=False))
-        model.add(Dropout(0.2))
-        model.add(Dense(25))
-        model.add(Dropout(0.2))
-        model.add(Dense(data.shape[1]))
+    elif flg_lstm_algo == 1:
+        # Split the data for lstm_cat training and testing
+        x1, x2, x3, y = stock_lib.prepare_data_3d(np_data, predict_window)
+        (x1_train, x1_test, x2_train, x2_test, x3_train, x3_test, y_train, y_test) = stock_lib.train_test_split_3d(x1,
+                                                                                                                   x2,
+                                                                                                                   x3,
+                                                                                                                   y)
+        # Scaling the training data
+        (x1_train, x1_test, x2_train, x2_test, x3_train, x3_test, y_train, y_test, sc) = stock_lib.minmaxscale_4d(
+            x1_train,
+            x1_test,
+            x2_train,
+            x2_test,
+            x3_train,
+            x3_test,
+            y_train,
+            y_test)
+        # Train the model
+        model = stock_lib.define_lstm_cat_model(x1_train, x2_train, x3_train)
         model.summary()
+        model = stock_lib.train_save_model(model, [x1_train, x2_train, x3_train], y_train, [x1_test, x2_test, x3_test],
+                                           y_test, epoch, flg_train_model, stock_symbol, dev_env)
+        predicted_p = stock_lib.get_past_prediction_3d(model, np_data, predict_window, sc)
+        predicted_f = stock_lib.get_future_prediction_3d(model, np_data, predict_window, sc)
+        print(predicted_p[:, 0])
 
-        # Compile and fit model
-        model.compile(loss='mse', optimizer='adam')
-        model.fit(x_train, y_train, epochs=train_epochs, batch_size=32, validation_data=(x_test, y_test),
-                  verbose=2)
-
-        # Prediction for past days
-        future_days_p = window_size * 2
-        last_window_p = np.array(data[-window_size * 2:-window_size])
-        predicted_p = []
-        for i in range(future_days_p):
-            next_day = np.array(
-                model.predict(last_window_p.reshape(1, last_window_p.shape[0], last_window_p.shape[1])))
-            predicted_p.append(next_day)
-            last_window_p = np.concatenate((last_window_p[-window_size + 1:, :], next_day), axis=0)
-
-        # Inverse transform the predicted values to their original scale
-        predicted_p = np.array(predicted_p)
-        predicted_p = predicted_p.reshape(-1, data.shape[1])
-        predicted_p = scaler.inverse_transform(predicted_p)
-
-        # Prediction for future days
-        future_days_f = window_size
-        last_window_f = np.array(data[-window_size:])
-        predicted_f = []
-        for i in range(future_days_f):
-            next_day = np.array(
-                model.predict(last_window_f.reshape(1, last_window_f.shape[0], last_window_f.shape[1])))
-            predicted_f.append(next_day)
-            last_window_f = np.concatenate((last_window_f[-window_size + 1:, :], next_day), axis=0)
-
-        # Inverse transform the predicted values to their original scale
-        predicted_f = np.array(predicted_f)
-        predicted_f = predicted_f.reshape(-1, data.shape[1])
-        predicted_f = scaler.inverse_transform(predicted_f)
-
-        # Print the predicted values
-        print(f'Past predicted closing: {predicted_p[:, 0]}')
-        print(f'Future predicted closing: {predicted_f[:, 0]}')
-        fig, ax = plt.subplots()
-        ax.plot(np.arange(-window_size + 1, window_size + 1), predicted_p[:, 0], color='red',
-                label='Predicted Past Closing')
-        ax.plot(np.arange(1, window_size + 1), predicted_f[:, 0], color='orange', label='Predicted Future Closing')
-        ax.plot(np.arange(-future_days_p + 1, 1), raw_data[['Close']][-future_days_p:], color='blue',
-                label='Actual Closing')
-        ax.plot(np.arange(-future_days_p + 1, 1), df['EMA10'][-future_days_p:], color='green', label='EMA10')
-        ax.plot(np.arange(-future_days_p + 1, 1), df['SMA20'][-future_days_p:], color='cyan', label='SMA20')
-        if earning_delta <= window_size:
-            plt.axvline(x=earning_delta, color='red')
-        ax2 = ax.twinx()
-        ax2.set_ylim([0, 100])
-        ax2.set_zorder(-100)
-        ax.set_facecolor('none')
-        ax2.scatter(np.arange(-future_days_p + 1, 1), df[['RSI14']][-future_days_p:], color='magenta', marker='x',
-                    s=10, label='RSI14')
-        plt.title(f"Predicted Market Price for {stock_name} in past {window_size} days")
-        ax.legend()
-        ax2.legend()
-        plt.grid()
-
-    elif flag_lstm_algo == 1:
-        # Define function to create merged-LSTM-DNN model
-        x1, x2, x3, y1 = prepare_data2(data, window_size, offset_value)
-        (x1_train, x1_test, y1_train, y1_test) = split_data(x1, y1, 0.2)
-        (x2_train, x2_test, y1_train, y1_test) = split_data(x2, y1, 0.2)
-        (x3_train, x3_test, y1_train, y1_test) = split_data(x3, y1, 0.2)
-
-        input1 = Input(shape=(x1_train.shape[1], x1_train.shape[2]))
-        input2 = Input(shape=(x2_train.shape[1], x2_train.shape[2]))
-        input3 = Input(shape=(x3_train.shape[1], x3_train.shape[2]))
-
-        lstm11 = LSTM(64, return_sequences=True)(input1)
-        lstm12 = LSTM(64, return_sequences=True)(input2)
-        lstm13 = LSTM(64, return_sequences=True)(input3)
-
-        lstm21 = LSTM(64, return_sequences=False)(lstm11)
-        lstm22 = LSTM(64, return_sequences=False)(lstm12)
-        lstm23 = LSTM(64, return_sequences=False)(lstm13)
-
-        combined_lstm = concatenate([lstm21, lstm22, lstm23])
-        d1 = Dense(75, activation='relu')(combined_lstm)
-        d1 = Dropout(0.2)(d1)
-        d2 = Dense(25, activation='relu')(d1)
-        d2 = Dropout(0.2)(d2)
-        output = Dense(data.shape[1])(d2)
-
-        model = keras.Model(inputs=[input1, input2, input3], outputs=output)
+    elif flg_lstm_algo == 2:
+        # Split the data for training and testing
+        x, y = stock_lib.prepare_data(np_data, data_window)
+        (x_train, x_test, y_train, y_test) = train_test_split(x, y, test_size=0.3)
+        # Scaling the training data
+        (x_train, x_test, y_train, y_test, sc) = stock_lib.minmaxscale_3d(x_train, x_test, y_train, y_test)
+        # Train the model
+        model = stock_lib.define_lstm_cnn_model(x_train)
         model.summary()
-        model.compile(loss='mse', optimizer='adam')
-        model.fit([x1_train, x2_train, x3_train], y1_train, epochs=train_epochs, batch_size=32,
-                  validation_data=([x1_test, x2_test, x3_test], y1_test), verbose=2)
+        model = stock_lib.train_save_model(model, x_train, y_train, x_test, y_test, epoch, flg_train_model,
+                                           stock_symbol, dev_env)
+        predicted_p = stock_lib.get_past_prediction(model, np_data, predict_window, data_window, sc)
+        predicted_f = stock_lib.get_future_prediction(model, np_data, predict_window, data_window, sc)
+        print(predicted_p[:, 0])
 
-        # Prediction for past days
-        future_days_p = window_size * 2
-        last_window_p1 = np.array(data[-window_size * 2:-window_size])
-        last_window_p2 = np.array(data[-window_size * 3:-window_size])
-        last_window_p3 = np.array(data[-window_size * 4:-window_size])
-        predicted_p = []
-        for i in range(future_days_p):
-            next_day = np.array(model.predict(
-                [last_window_p1.reshape(1, last_window_p1.shape[0], last_window_p1.shape[1]),
-                 last_window_p2.reshape(1, last_window_p2.shape[0], last_window_p2.shape[1]),
-                 last_window_p3.reshape(1, last_window_p3.shape[0], last_window_p3.shape[1])]))
-            predicted_p.append(next_day)
-            last_window_p1 = np.concatenate((last_window_p1[-window_size + 1:, :], next_day), axis=0)
-            last_window_p2 = np.concatenate((last_window_p2[-window_size * 2 + 1:, :], next_day), axis=0)
-            last_window_p3 = np.concatenate((last_window_p3[-window_size * 3 + 1:, :], next_day), axis=0)
-
-        # Inverse transform the predicted values to their original scale
-        predicted_p = np.array(predicted_p)
-        predicted_p = predicted_p.reshape(-1, data.shape[1])
-        predicted_p = scaler.inverse_transform(predicted_p)
-
-        # Prediction for future days
-        future_days_f = window_size
-        last_window_f1 = np.array(data[-window_size:])
-        last_window_f2 = np.array(data[-window_size * 2:])
-        last_window_f3 = np.array(data[-window_size * 3:])
-        predicted_f = []
-        for i in range(future_days_f):
-            next_day = np.array(model.predict(
-                [last_window_f1.reshape(1, last_window_f1.shape[0], last_window_f1.shape[1]),
-                 last_window_f2.reshape(1, last_window_f2.shape[0], last_window_f2.shape[1]),
-                 last_window_f3.reshape(1, last_window_f3.shape[0], last_window_f3.shape[1])]))
-            predicted_f.append(next_day)
-            last_window_f1 = np.concatenate((last_window_f1[-window_size + 1:, :], next_day), axis=0)
-            last_window_f2 = np.concatenate((last_window_f2[-window_size * 2 + 1:, :], next_day), axis=0)
-            last_window_f3 = np.concatenate((last_window_f3[-window_size * 3 + 1:, :], next_day), axis=0)
-
-        # Inverse transform the predicted values to their original scale
-        predicted_f = np.array(predicted_f)
-        predicted_f = predicted_f.reshape(-1, data.shape[1])
-        predicted_f = scaler.inverse_transform(predicted_f)
-
-        # Print the predicted values
-        print(f'Past predicted closing: {predicted_p[:, 0]}')
-        print(f'Future predicted closing: {predicted_f[:, 0]}')
-        fig, ax = plt.subplots()
-        ax.plot(np.arange(-window_size + 1, window_size + 1), predicted_p[:, 0], color='red',
-                label='Predicted Past Closing')
-        ax.plot(np.arange(1, window_size + 1), predicted_f[:, 0], color='orange', label='Predicted Future Closing')
-        ax.plot(np.arange(-future_days_p + 1, 1), raw_data[['Close']][-future_days_p:], color='blue',
-                label='Actual Closing')
-        ax.plot(np.arange(-future_days_p + 1, 1), df['EMA10'][-future_days_p:], color='green', label='EMA10')
-        ax.plot(np.arange(-future_days_p + 1, 1), df['SMA20'][-future_days_p:], color='cyan', label='SMA20')
-        if earning_delta <= window_size:
-            plt.axvline(x=earning_delta, color='red')
-        ax2 = ax.twinx()
-        ax2.set_ylim([0, 100])
-        ax2.set_zorder(-100)
-        ax.set_facecolor('none')
-        ax2.scatter(np.arange(-future_days_p + 1, 1), df[['RSI14']][-future_days_p:], color='magenta', marker='x',
-                    s=10, label='RSI14')
-        plt.title(f"Predicted Market Price for {stock_name} in past {window_size} days")
-        ax.legend()
-        ax2.legend()
-        plt.grid()
-
-    filename = f"{end_date.year}.{end_date.month}.{end_date.day}_{stock_name}_{flag_lstm_algo}.png"
-    folder = f"/PyCharm/2023.04_StockPredict_LSTM/prt_scr/{stock_name}"
-    if not os.path.exists(folder):
-        # If it doesn't exist, create the directory
-        os.makedirs(folder)
-    plt.savefig(folder + "/" + filename)
-    plt.show()
+    # Plot the results
+    stock_lib.plot_prediction(stock_symbol, raw_data, predict_window, predicted_p, predicted_f, earning_delta)
+    stock_lib.save_graph(end_date, stock_symbol, flg_lstm_algo, dev_env)
 
 
 # Create GUI window
@@ -408,7 +227,7 @@ RB_past_future_a.place(x=500, y=20)
 RB_past_future_b = tk.Radiobutton(gui, text="CAT.LSTM", variable=var_lstm_algo, value=1)
 RB_past_future_b.pack(anchor="w")
 RB_past_future_b.place(x=500, y=50)
-RB_past_future_c = tk.Radiobutton(gui, text="Batch", variable=var_lstm_algo, value=2)
+RB_past_future_c = tk.Radiobutton(gui, text="CNN.LSTM", variable=var_lstm_algo, value=2)
 RB_past_future_c.pack(anchor="w")
 RB_past_future_c.place(x=500, y=80)
 
